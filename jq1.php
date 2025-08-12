fa006132fad9bc733eeafadcad120f47713e1bc3
mari75a <sangeethlakshan0@gmail.com>
2025-08-12 22:57:34 +0530

    add jquery

---
+++ jq1.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<title> jQuery Tutorial </title>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js">
+</script>
+</head>
+<body>
+<h1 id="tutorial">jQuery Tutorial</h1>
+<p>My jQuery Exercise</p>
+
+<script>
+    $('#tutorial').click(function(){
+           $(this).css('color','red');
+    });
+</script>
+</body>
+</html>
